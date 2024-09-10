2f07f5d43b2c5202c1b0d67b1a6443b7d5e763a8
eswarbilla <eswarbilla254@gmail.com>
2024-09-10 09:39:31 +0530

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">
+                Description
+            </div>
+            <div className='descriptionbox-nav-box fade'>
+                Reviews (122)
+            </div>
+        </div>
+<div className="descriptionbox-description">
+    <p>Welcome to Ecommerce, where shopping meets convenience and quality. Dive into a diverse selection of products spanning electronics, fashion, home decor, and more. Our intuitive platform ensures a seamless browsing experience, with detailed product descriptions and customer reviews to guide your purchase decisions. Enjoy peace of mind with secure transactions and swift, reliable shipping.</p>
+<p> your premier destination for online shopping excellence. Explore an extensive collection of top-notch products, ranging from cutting-edge  stylish fashion pieces and everything in between. </p>
+</div>
+    </div>
+  )
+}
+
+export default DescriptionBox
